cea3da5878ddca9eeedb2d60bb4d07d78c84a2db
ChiragChrg <chiruchirag2001@gmail.com>
2023-05-14 00:11:27 +0530

    Updated Custom cursor

--- src/Components/Cursor.jsx
+++ src/Components/Cursor.jsx
@@ -2,23 +2,33 @@ import { useEffect, useRef } from 'react'
 
 const Cursor = () => {
     const CursorRef = useRef();
+    const CursorDotRef = useRef();
 
     useEffect(() => {
         document.addEventListener('mousemove', evt => {
             const x = evt.clientX - CursorRef.current.offsetWidth / 2;
             const y = evt.clientY - CursorRef.current.offsetHeight / 2;
+            const xDot = evt.clientX - CursorDotRef.current.offsetWidth / 2;
+            const yDot = evt.clientY - CursorDotRef.current.offsetHeight / 2;
 
-            // CursorRef.current.style.top = y + "px";
-            // CursorRef.current.style.left = x + "px";
+            // CursorDotRef.current.style.top = y + "px";
+            // CursorDotRef.current.style.left = x + "px";
             // CursorRef.current.style.setProperty("--posx", x + "px")
             // CursorRef.current.style.setProperty("--posy", x + "px")
 
             const keyframes = {
                 transform: `translate(${x}px, ${y}px)`
             }
+            const DotKeyframes = {
+                transform: `translate(${xDot}px, ${yDot}px)`
+            }
 
             CursorRef.current.animate(keyframes, {
                 duration: 800,
+                fill: "forwards"
+            })
+            CursorDotRef.current.animate(DotKeyframes, {
+                duration: 10,
                 fill: "forwards"
             })
         })
@@ -28,6 +38,7 @@ const Cursor = () => {
     return (
         <div className="Cursor-Main">
             <div id="cursor" ref={CursorRef}></div>
+            <div id="cursorDot" ref={CursorDotRef}></div>
         </div>
     )
 }
