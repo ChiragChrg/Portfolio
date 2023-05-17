1d6958d4ddfd8da97957032ac14ab3a46d49a6df
ChiragChrg <chiruchirag2001@gmail.com>
2023-05-17 21:01:28 +0530

    Added Framer motion to Hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,44 @@
 import "./Hero.css"
-import { useRef } from 'react'
+import { motion } from "framer-motion"
 
 import AOTImg from "../../../public/AOT.jpg"
 // import AOTImg from "../../Assets/AOT.jpg"
 
 const Hero = () => {
-    const AvatarRef = useRef();
+    const orbitVariant = {
+        hidden: {
+            opacity: 0
+        },
+        visible: {
+            opacity: 1,
+            transition: {
+                delay: 0.6,
+                delayChildren: 0.6,
+                staggerChildren: 0.1
+            }
+        }
+    }
+
+    const circleVariant = {
+        hidden: {
+            opacity: 0,
+            scale: 0.8
+        },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            transition: {
+                ease: [0, 0.71, 0.2, 1.01],
+                duration: 0.5,
+                scale: {
+                    type: "spring",
+                    damping: 8,
+                    stiffness: 100,
+                    restDelta: 0.001
+                }
+            }
+        }
+    }
 
     return (
         <div className="Hero-Main flex">
@@ -15,25 +48,37 @@ const Hero = () => {
                 </h1>
             </div>
 
-            <div className="Hero-AvatarContainer flex">
-                <div className="Hero-AvatarTrail Trail1">
-                    <div className="Hero-TrailDot"></div>
-                </div>
-                <div className="Hero-AvatarTrail Trail2">
-                    <div className="Hero-TrailDot"></div>
-                </div>
-                <div className="Hero-AvatarTrail Trail3">
-                    <div className="Hero-TrailDot"></div>
-                </div>
-                <div className="Hero-AvatarTrail Trail4">
-                    <div className="Hero-TrailDot"></div>
-                </div>
-
-                <div className="Hero-Avatar flex" ref={AvatarRef}>
+            <motion.div
+                variants={orbitVariant}
+                initial="hidden"
+                animate="visible"
+                className="Hero-AvatarContainer flex">
+                <motion.div
+                    variants={circleVariant}
+                    className="Hero-Avatar flex">
                     <img src={AOTImg} alt="AOTImg" />
-                </div>
-
-            </div>
+                </motion.div>
+                <motion.div
+                    variants={circleVariant}
+                    className="Hero-AvatarTrail Trail1">
+                    <div className="Hero-TrailDot"></div>
+                </motion.div>
+                <motion.div
+                    variants={circleVariant}
+                    className="Hero-AvatarTrail Trail2">
+                    <div className="Hero-TrailDot"></div>
+                </motion.div>
+                <motion.div
+                    variants={circleVariant}
+                    className="Hero-AvatarTrail Trail3">
+                    <div className="Hero-TrailDot"></div>
+                </motion.div>
+                <motion.div
+                    variants={circleVariant}
+                    className="Hero-AvatarTrail Trail4">
+                    <div className="Hero-TrailDot"></div>
+                </motion.div>
+            </motion.div>
         </div>
     )
 }
