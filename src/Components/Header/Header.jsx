f6453b0375e4b071fc35641f9c55a4cd7d2bda8a
ChiragChrg <chiruchirag2001@gmail.com>
2023-07-02 17:42:56 +0530

    Nav Minor Fixes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -50,7 +50,10 @@ const Header = () => {
                 </nav>
 
                 <a className="Header-ActionBtn flex" href="https://raw.githubusercontent.com/ChiragChrg/ChiragChrg.github.io/main/Chirag%20Resume%20Dummy.pdf" download>
-                    <div>Resume</div>
+                    <div className="flex gap05">
+                        <p>Resume</p>
+                        <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1.2em" width="1.2em" xmlns="http://www.w3.org/2000/svg"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
+                    </div>
                 </a>
             </div>
         </header>
--- src/Components/MobileNav/MobileNav.jsx
+++ src/Components/MobileNav/MobileNav.jsx
@@ -136,7 +136,10 @@ const MobileNav = () => {
                     className="MobileNav-LinkHolder flex">
                     <a className="MobileNav-ActionBtn flex"
                         onClick={() => setOpenNav(false)} href="https://raw.githubusercontent.com/ChiragChrg/ChiragChrg.github.io/main/Chirag%20Resume%20Dummy.pdf" download>
-                        <div>Resume</div>
+                        <div className="flex gap">
+                            <p>Resume</p>
+                            <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1.2em" width="1.2em" xmlns="http://www.w3.org/2000/svg"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
+                        </div>
                     </a>
                 </motion.div>
             </motion.nav>
