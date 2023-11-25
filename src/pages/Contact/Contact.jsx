462af0f7964c28bff12896d50e851f46c2140e9b
ChiragChrg <chiruchirag2001@gmail.com>
2023-11-25 17:41:37 +0530

    Modified Contacts Email Copy Btn UI ✨

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -110,7 +110,24 @@ const Contact = () => {
                         </a>
                     </div>
                     <img className="Contact-VectorArt" src={ContactArt} alt="ContactArt" width="400px" />
-                    <div className="Contact-EmailId" onClick={CopyMail}>{copyText}</div>
+                    <div className="Contact-EmailId flex gap" onClick={CopyMail}>
+                        <span>{copyText}</span>
+                        {copyText == "chiruchirag2001@gmail.com" && <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            fill="currentColor"
+                            stroke="currentColor"
+                            strokeWidth="0"
+                            viewBox="0 0 24 24"
+                        >
+                            <path fill="none" stroke="none" d="M0 0h24v24H0z"></path>
+                            <path
+                                stroke="none"
+                                d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"
+                            ></path>
+                        </svg>}
+                    </div>
                 </FadeUp>
 
                 <FadeUp width="100%" className="Contact-Form flex col">
