e11bf6f6ae52a62dc9711bb775642c7dde4e2727
ChiragChrg <chiruchirag2001@gmail.com>
2024-01-31 21:21:52 +0530

    Added Public Files

---
+++ public/ServiceWorker.js
@@ -0,0 +1,44 @@
+const CacheName = "Chrg-V-1.5";
+const CacheList = [
+    "Icons/144.png",
+    "Icons/192.png",
+    "Icons/512.png",
+    "favicon.png",
+];
+
+//Installing Service Worker
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CacheName).then((cache) => {
+            console.log("Checking Cache");
+            return cache.addAll(CacheList);
+        })
+    );
+});
+
+//Fetching Service Worker
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
+
+//Activating Service Worker
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CacheName) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
