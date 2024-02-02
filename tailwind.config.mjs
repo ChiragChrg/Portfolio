7322104b66b1a5810b25599802f45bbab2888752
ChiragChrg <chiruchirag2001@gmail.com>
2024-02-02 14:38:21 +0530

    Modified Config files

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily:{
+				poppins:"var(--astro-font-poppins)",
+				satisfy:"var(--astro-font-satisfy)",
+				ubuntu:"var(--astro-font-ubuntu)",
+			},
+			backgroundImage:{
+				'headerGradient': 'var(--headerGradient)',
+			}
+		},
 	},
 	plugins: [],
 }
