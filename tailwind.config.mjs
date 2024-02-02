c6ed8d5722141f22c982fa4d421f068e4d1f80d8
ChiragChrg <chiruchirag2001@gmail.com>
2024-02-03 00:27:53 +0530

    Modified Tailwind congif

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,16 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			colors:{
+				background: "var(--base)",
+				foreground:"var(--text)",
+				purple:"var(--purple)",
+				pink:"var(--pink)",
+				purpink:"var(--purpink)",
+				purpleLite:"var(--purpleLite)",
+				purpleLiteAlt:"var(--purpleLiteAlt)",
+				pinkLite:"var(--pinkLite)",
+			},
 			fontFamily:{
 				poppins:"var(--astro-font-poppins)",
 				satisfy:"var(--astro-font-satisfy)",
@@ -10,15 +20,21 @@ export default {
 			},
 			backgroundImage:{
 				'headerGradient': 'var(--headerGradient)',
+				'OrbitGradient': 'var(--orbitGradient)',
 			},
 			keyframes: {
 				"ripple": {
 				  from: { opacity: 1, scale: 0 },
 				  to: { opacity: 0, scale: 1 },
 				},
+				"orbit": {
+				  from: {rotate:"0deg"},
+				  to: { rotate:"360deg"},
+				},
 			},
 			animation: {
 				"ripple": "ripple 1s infinite",
+				"orbit": "orbit 6s linear infinite",
 			},
 		},
 	},
