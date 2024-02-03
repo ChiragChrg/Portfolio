e9fabf31a09166bc7b475e8c76dcf96ab8b700b3
ChiragChrg <chiruchirag2001@gmail.com>
2024-02-03 17:10:16 +0530

    Removed package: Astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), sitemap()]
+  integrations: [react(), tailwind()]
 });
