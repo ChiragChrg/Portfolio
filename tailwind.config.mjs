8aa81b0364677bf11e33dfd54117c739babd06f8
ChiragChrg <chiruchirag2001@gmail.com>
2024-02-04 01:04:04 +0530

    Footer section Done 👍🏻🚀

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,6 +12,7 @@ export default {
 				purpleLite:"var(--purpleLite)",
 				purpleLiteAlt:"var(--purpleLiteAlt)",
 				pinkLite:"var(--pinkLite)",
+				footer:"var(--footer)",
 			},
 			fontFamily:{
 				poppins:"var(--astro-font-poppins)",
