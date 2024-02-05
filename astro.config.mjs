c66b4edbc8aebcb798aadaa5e8fd263b3faf20c4
ChiragChrg <chiruchirag2001@gmail.com>
2024-02-05 22:08:02 +0530

    Removed package: astro-compress as it keeps on failing during BUILD

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind(),
-    compress({
-      CSS: false,
-      HTML: false,
-      Image: false,
-      JavaScript: false,
-      SVG: true,
-    })]
+  integrations: [react(), tailwind()]
 });
