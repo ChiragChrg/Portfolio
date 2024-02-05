1abc4fce5264b0ada9688c65ad1f40b4ce1096c4
ChiragChrg <chiruchirag2001@gmail.com>
2024-02-05 20:55:18 +0530

    Added package: astro-compress to optimize files on build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [
+    react(), 
+    tailwind(), 
+    compress({
+    Path:["./dist", "./_astro", "./Compress"]
+  })]
 });
