5a779cb10d7398030715e1f54022386fa72d11ae
ChiragChrg <chiruchirag2001@gmail.com>
2024-02-05 22:23:48 +0530

    Exporting components as Named-exports

---
+++ src/components/React/index.ts
@@ -0,0 +1,6 @@
+export { default as Header } from "./Header"
+export { default as MobileHeader } from "./MobileHeader"
+export { default as SkillGrid } from "./SkillGrid"
+export { default as ContactForm } from "./ContactForm"
+export { default as Cursor } from "./Cursor"
+export { default as FadeUp } from "./FadeUp"
