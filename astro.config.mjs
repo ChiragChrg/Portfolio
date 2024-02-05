b6ebe1b609a2f780262696824b9b84d131266592
ChiragChrg <chiruchirag2001@gmail.com>
2024-02-05 22:01:30 +0530

    Updated astro-compress to exclude files with dynamic import

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,18 @@ import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), compress()]
+  integrations: [
+    react(),
+    tailwind(),
+    compress({
+      CSS: false,
+      HTML: false,
+      Image: false,
+      JavaScript: true,
+      SVG: true,
+      Exclude: [
+        (File) => File.includes("FadeUp"),
+        (File) => File.includes("HeroMotion"),
+      ],
+    })]
 });
