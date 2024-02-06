cb6ad8ca03c1e7c14b3580a4ebcd818529b28d73
ChiragChrg <chiruchirag2001@gmail.com>
2024-02-06 20:43:41 +0530

    Exporting astro components as Named-exports

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Hero } from "./Hero.astro"
+export { default as About } from "./About.astro"
+export { default as Skills } from "./Skills.astro"
+export { default as Contact } from "./Contact.astro"
+export { default as Footer } from "./Footer.astro"
