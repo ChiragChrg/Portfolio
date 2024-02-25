639daa728fbb3cb98706d6d5b3f17808843cb6f5
ChiragChrg <chiruchirag2001@gmail.com>
2024-02-25 21:16:04 +0530

    Added Netlify Adapeter for Hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [preact(), tailwind()]
+  integrations: [preact(), tailwind()],
+  adapter: netlify()
 });
