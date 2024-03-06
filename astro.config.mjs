c7425e1aa8d3f87d5f98d32e1f0dfc3da4a7fe60
ChiragChrg <chiruchirag2001@gmail.com>
2024-03-06 22:44:15 +0530

    Removed package: partyTown- throttles Ui interation, added astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify";
-
-import partytown from "@astrojs/partytown";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [preact(), tailwind(), partytown()],
+  integrations: [preact(), tailwind(), compress()],
   adapter: netlify()
 });
