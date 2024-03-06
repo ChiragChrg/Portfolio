ccfc86752994b461c9b20a4dba05fbccb23852ce
ChiragChrg <chiruchirag2001@gmail.com>
2024-03-06 22:19:44 +0530

    Added package:  PartyTown to handle work intensive scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [preact(), tailwind()],
+  integrations: [preact(), tailwind(), partytown()],
   adapter: netlify()
 });
