967c3fe1fea5217fc7889c8a8ce46b62e1135acf
ChiragChrg <chiruchirag2001@gmail.com>
2025-10-20 15:08:15 +0530

    chore: :wrench: Renamed portfolio config file

--- src/portfolio.config.ts
+++ src/portfolio.config.ts
@@ -1,24 +1,25 @@
-import { Avatar_180 } from "@assets/images";
+import { Avatar_180, Avatar_250 } from "@assets/images";
 import {
     DiscordIcon,
-    FaceBookIcon,
     GithubIcon,
     InstagramIcon,
     LinkedinIcon,
     LogoIcon,
     TwitterIcon,
-    GmailIcon
+    GoogleIcon
 } from "@assets/SVGs";
 
-export const portfolioConfig = {
-    // Header Section
+// Header Section
+export const headerConfig = {
     logotext: "Portfolio.",
     actionButton: {
         text: "Resume",
         url: "/Chirag Resume.pdf"
     },
+}
 
-    // Hero Section
+// Hero Section
+export const heroConfig = {
     firstName: "Chirag",
     lastName: ".",
     position: "Fullstack Software Engineer",
@@ -27,66 +28,83 @@ export const portfolioConfig = {
         highlightedText: "Exciting Stuff",
         suffixText: "on the Internet."
     },
-    heroAvatar: Avatar_180,
-    links: {
-        github: {
-            url: "https:github.com/ChiragChrg",
+    avatar: Avatar_180,
+    links: [
+        {
+            label: "GitHub",
+            url: "https://github.com/ChiragChrg",
             icon: GithubIcon
         },
-        linkedin: {
+        {
+            label: "LinkedIn",
             url: "https://www.linkedin.com/in/chiragchrg/",
             icon: LinkedinIcon
         },
-        twitter: {
+        {
+            label: "X",
             url: "https://x.com/ChiragChrg",
             icon: TwitterIcon
         },
-        instagram: {
+        {
+            label: "Instagram",
             url: "https://www.instagram.com/chiragchrg/",
             icon: InstagramIcon
         },
-        facebook: {
-            url: "https://www.facebook.com/chiragchrg",
-            icon: FaceBookIcon
-        },
-        email: {
-            url: "mailto:chiruchirag2001@gmail.com",
-            icon: GmailIcon
+        {
+            label: "Google Search",
+            url: "https://www.google.com/search?q=ChiragChrg",
+            icon: GoogleIcon
         }
-    },
+    ],
+}
 
-    // About Section
-    aboutAvatar: Avatar_180,
+// About Section
+export const aboutConfig = {
+    aboutAvatar: Avatar_250,
     description: [
         "I'm Chirag, a full stack web developer with a strong passion for crafting digital realms that captivate and inspire.",
         "I specialize in creating dynamic and interactive UI/UX experience. I stay up to date with the latest tools and techniques and worked on a wide range of projects, from personal websites to large-scale applications, sharpening  problem solving skills and fostering creativity.",
-        " If you're seeking a dedicated and passionate web developer, I'm eager to collaborate with you. Let's bring your ideas life!"
+        "If you're seeking a dedicated and passionate web developer, I'm eager to collaborate with you. Let's bring your ideas life!"
     ],
+}
 
-    // Footer Section
-    footerLogo: LogoIcon,
-    footerText: "ChiragChrg | Portfolio.",
-    footerLinks: {
-        github: {
+// Footer Section
+export const footerConfig = {
+    logo: LogoIcon,
+    title: "ChiragChrg | Portfolio.",
+    links: [
+        {
+            label: "GitHub",
             url: "https:github.com/ChiragChrg",
             icon: GithubIcon
         },
-        linkedin: {
+        {
+            label: "LinkedIn",
             url: "https://www.linkedin.com/in/chiragchrg/",
             icon: LinkedinIcon
         },
-        twitter: {
+        {
+            label: "X",
             url: "https://x.com/ChiragChrg",
             icon: TwitterIcon
         },
-        instagram: {
+        {
+            label: "Instagram",
             url: "https://www.instagram.com/chiragchrg/",
             icon: InstagramIcon
         },
-        discord: {
-            url: "https://discord.gg/your-discord-link",
+        {
+            label: "Discord",
+            url: "https://discord.gg/WmVQjMryDz",
             icon: DiscordIcon
+        },
+        {
+            label: "Google Search",
+            url: "https://www.google.com/search?q=ChiragChrg",
+            icon: GoogleIcon
         }
-    },
-    copyrightText: `© Copyright 2023-${new Date().getFullYear()} ChiragChrg`
-};
+    ],
+    copyrightText: `© Copyright 2023-${new Date().getFullYear()
+        } ChiragChrg`
+}
+
